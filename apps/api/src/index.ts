f14f76a5c4a6361c0bb150ec2a6d7d0f44bde65a
muazzam <muazzamkhokhar.mk@gmail.com>
2025-11-19 13:45:42 +0500

    connect with db and created schema and user API

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,14 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import healthRoutes from './routes/health.js'; // <- use .ts here
-
+import mongoose from 'mongoose';
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
 app.use(express.json());
-app.use('/v1/health', healthRoutes);
+app.use(cors());
+dotenv.config({ debug: true });
 
-app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+
+app.get('/',(req,res)=>{
+  res.json({message:"First Api for smartlearning"})
+})
+app.use("/api/users", userRoutes);
+
+app.use('/v1/health', healthRoutes);
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || '';
+
+mongoose
+      .connect(MONGO_URI)
+      .then(()=>{
+        console.log("Mongo DB Connected")
+        app.listen(PORT,()=>console.log(`API running on http://localhost:${PORT}`));
+      })
+      .catch((err)=>console.log(`MongoDB Error: ${err}`))
+
+
---
+++ apps/api/src/routes/user.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import User from "../models/User.js";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+export default router;
